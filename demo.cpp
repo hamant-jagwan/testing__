3db73864eb3f6d2c85e75bb7f8e5ad1defd488ec
Hamant <hemant7jagwan@gmail.com>
2024-08-10 17:34:20 +0530

    faltu ka commit

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    int arr[] = {1,35,6,3,7,8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for(int i =0 ; i <n;i++){
+        cout<< "the value in the index " << i << " is " << arr[i]<<endl;
+    }
+    return 0;
+}
